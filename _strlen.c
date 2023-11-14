a655b653e6fe2b4c305dd4bf097dbf722ffedbe7
Alaa <aemm7682@gmail.com>
2023-11-14 14:31:28 -0800

    update

---
+++ _strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string
+ *
+ * @s: variable
+ *
+ * Return: length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0 ; *s != '\0' ; s++)
+		i++;
+	return (i);
+}
