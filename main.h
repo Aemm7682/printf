28bbe47e39eb23747f61800f1dacd7196201d736
Alaa <aemm7682@gmail.com>
2023-11-14 13:38:00 -0800

    _printf

---
+++ main.h
@@ -0,0 +1,103 @@
+#ifndef _PRINTF_H
+#define _PRINTF_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+#define NULL_string "(null)"
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+#define CONVERT_LOWERCASE 1
+#define CONVERT_UNSIGNED 2
+
+/**
+ * struct parameters - parameters struct
+ * @unsign: flag if unsigned value
+ * @plus_flag: on if _flag specified
+ * @space_flag: on if hashtag_flag specified
+ * @hashtag_flag: on if flag specified
+ * @zero_flag: on if _flag specified
+ * @minus_flag: on if _flag specified
+ * @width: field width specified
+ * @precision: field precision specified
+ * @h_modifier: on if h_modifier is specified
+ * @l_modifier: on if l_modifier is specified
+ */
+typedef struct parameters
+{
+	unsigned int unsign    : 1;
+	unsigned int plus_flag : 1;
+	unsigned int space_flag: 1;
+	unsigned int hashtag_flag : 1;
+	unsigned int zero_flag   : 1;
+	unsigned int minus_flag  : 1;
+	unsigned int width;
+	unsigned int precision;
+	unsigned int h_modifier : 1;
+	unsigned int l_modifier : 1;
+} params_t;
+/**
+ * struct specifier - struct
+ * @specifier: specifier
+ * @f: function
+ */
+typedef struct specifier
+{
+	char *specifier;
+	int (*f)(va_list, params_t *);
+} specifier_t;
+
+/* function to print */
+int _puts(char *str);
+int _putchar(int c);
+
+/* print function for the type like char/int ..*/
+int _print_char(va_list ap, params_t *params);
+int _print_int(va_list ap, params_t *params);
+int _print_string(va_list ap, params_t *params);
+int _print_percent(va_list ap, params_t *params);
+int _print_S(va_list ap, params_t *params);
+
+/* numbers.c module */
+char *convert(long int num, int base, int flags, params_t);
+int print_unsigned(va_list ap, params_t *params);
+int print_address(va_list ap, params_t *params);
+
+/* specifier sections */
+int (*get_specifier(char *s))(va_list ap, params_t *params);
+int get_print_func(char *s, va_list ap, params_t *params);
+int get_flag(char *s, params_t *params);
+int get_modifier(char *s, params_t *params);
+int *get_width(char *s, params_t *params, va_list ap);
+
+/* convert number section */
+int print_hex(va_list ap, params_t *params);
+int print_HEX(va_list ap, params_t *params);
+int print_binary(va_list ap, params_t *params);
+int print_octal(va_list ap, params_t *params);
+
+/* simple printers section */
+int print_from_to(char *start, char *stop, char *except);
+int print_rev(va_list ap, params_t *params);
+int print_rot13(va_list ap, params_t *params);
+
+/* print_numbers section */
+int _isdigit(int c);
+int _strlen(char *s);
+int print_number(char *str, params_t *params);
+int print_number_right_shift(char *str, params_t *params);
+int print_number_left_shift(char *str, params_t *params);
+
+/* params.c section */
+void init_params(params_t *params, va_list ap);
+
+/* string_fields.c */
+char *get_precision(char *p, params_t *params, va_list ap);
+
+/* printf.c sction */
+int _printf(const char *format, ...);
+
+#endif
---
+++ params.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * init_params - clears struct fields and reset buf
+ * @params: the parameters struct
+ * @ap: the argment pointer
+ * Return: void
+ */
+void init_params
